67389f4fe830254c192d8ad0a0df87694bc40caf
lazzieecoderr <chakravarthyrrc@gmail.com>
2024-05-02 12:56:25 +0530

    completed

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const App = () => {
+const data=[
+  {
+   title:"FREE",
+   price:"$0/month",
+   user:"single-user",
+   storage:"50GB storage",
+   project:"Unlimited Public Project",
+   community:"Community Access",
+   projectIcon:"fas fa-check",
+   text:"text-muted"
+ }
+
+]
+
+
+
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default App;
